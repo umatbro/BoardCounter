07da49582db62a21bb4d84ddaad9c2358f5fb7e0
umatbro <umatbroo@gmail.com>
2017-02-23 20:48:27 +0100

    v0.1

---
+++ app/src/main/java/com/example/android/boardcounter/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.android.boardcounter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int playerAmount = 4;
+    LinearLayout background[] = new LinearLayout [playerAmount];
+    TextView playerName[] = new TextView [playerAmount];
+    TextView scoreTextView[] = new TextView [playerAmount];
+    Button sit[] = new Button [playerAmount];
+    Button stand[] = new Button [playerAmount];
+    Button minus5[] = new Button [playerAmount];
+    Button minus1[] = new Button [playerAmount];
+    Button plus1[] = new Button [playerAmount];
+    Button plus5[] = new Button [playerAmount];
+    Button reset[] = new Button [playerAmount];
+
+    PlayerCell player[] = new PlayerCell [playerAmount];
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //initializing all view variables
+        //for PLAYER 1
+        background[0] = (LinearLayout) findViewById(R.id.player1_background);
+        playerName[0] = (TextView) findViewById(R.id.player1_name);
+        scoreTextView[0] = (TextView) findViewById(R.id.player1_score);
+        sit[0] = (Button) findViewById(R.id.player1_sit);
+        stand[0] = (Button) findViewById(R.id.player1_stand);
+        minus5[0] = (Button) findViewById(R.id.player1_minus5);
+        minus1[0] = (Button) findViewById(R.id.player1_minus1);
+        plus1[0] = (Button) findViewById(R.id.player1_plus1);
+        plus5[0] = (Button) findViewById(R.id.player1_plus5);
+        reset[0] = (Button) findViewById(R.id.player1_reset);
+
+        //for PLAYER 2
+        background[1] = (LinearLayout) findViewById(R.id.player2_background);
+        playerName[1] = (TextView) findViewById(R.id.player2_name);
+        scoreTextView[1] = (TextView) findViewById(R.id.player2_score);
+        sit[1] = (Button) findViewById(R.id.player2_sit);
+        stand[1] = (Button) findViewById(R.id.player2_stand);
+        minus5[1] = (Button) findViewById(R.id.player2_minus5);
+        minus1[1] = (Button) findViewById(R.id.player2_minus1);
+        plus1[1] = (Button) findViewById(R.id.player2_plus1);
+        plus5[1] = (Button) findViewById(R.id.player2_plus5);
+        reset[1] = (Button) findViewById(R.id.player2_reset);
+
+        //for PLAYER 3
+        background[2] = (LinearLayout) findViewById(R.id.player3_background);
+        playerName[2] = (TextView) findViewById(R.id.player3_name);
+        scoreTextView[2] = (TextView) findViewById(R.id.player3_score);
+        sit[2] = (Button) findViewById(R.id.player3_sit);
+        stand[2] = (Button) findViewById(R.id.player3_stand);
+        minus5[2] = (Button) findViewById(R.id.player3_minus5);
+        minus1[2] = (Button) findViewById(R.id.player3_minus1);
+        plus1[2] = (Button) findViewById(R.id.player3_plus1);
+        plus5[2] = (Button) findViewById(R.id.player3_plus5);
+        reset[2] = (Button) findViewById(R.id.player3_reset);
+
+        //for PLAYER 4
+        background[3] = (LinearLayout) findViewById(R.id.player4_background);
+        playerName[3] = (TextView) findViewById(R.id.player4_name);
+        scoreTextView[3] = (TextView) findViewById(R.id.player4_score);
+        sit[3] = (Button) findViewById(R.id.player4_sit);
+        stand[3] = (Button) findViewById(R.id.player4_stand);
+        minus5[3] = (Button) findViewById(R.id.player4_minus5);
+        minus1[3] = (Button) findViewById(R.id.player4_minus1);
+        plus1[3] = (Button) findViewById(R.id.player4_plus1);
+        plus5[3] = (Button) findViewById(R.id.player4_plus5);
+        reset[3] = (Button) findViewById(R.id.player4_reset);
+
+        for (int i = 0 ; i < playerAmount; i++)
+            player[i] = new PlayerCell(
+                    background[i],
+                    playerName[i],
+                    scoreTextView[i],
+                    sit[i],
+                    stand[i],
+                    minus5[i],
+                    minus1[i],
+                    plus1[i],
+                    plus5[i],
+                    reset[i]);
+        //set background colors for all fields
+        player[0].setBackgroundColor("#ff4444");
+        player[1].setBackgroundColor("#ffbb33");
+        player[2].setBackgroundColor("#33b5e5");
+        player[3].setBackgroundColor("#99cc00");
+    }
+}
---
+++ app/src/main/java/com/example/android/boardcounter/PlayerCell.java
@@ -0,0 +1,138 @@
+package com.example.android.boardcounter;
+
+import android.graphics.Color;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by umat on 20.02.17.
+ */
+
+public class PlayerCell {
+    private int score;
+    private LinearLayout background;
+    private TextView playerName;
+    private TextView scoreTextView;
+    private Button buttonSit;
+    private Button buttonStand;
+    private Button buttonMinus5;
+    private Button buttonMinus1;
+    private Button buttonPlus1;
+    private Button buttonPlus5;
+    private Button resetButton;
+    private int backgroundColor;    // color of the layout background
+
+    public PlayerCell(LinearLayout background, TextView playerName, TextView scoreTextView, Button buttonSit, Button buttonStand, Button minus5, Button minus1, Button plus1, Button plus5, Button reset){
+        this.score = 0;
+        this.background = background;
+        this.playerName = playerName;
+        this.scoreTextView = scoreTextView;
+        this.buttonSit = buttonSit;
+        this.buttonStand = buttonStand;
+        this.buttonMinus5 = minus5;
+        this.buttonMinus1 = minus1;
+        this.buttonPlus1 = plus1;
+        this.buttonPlus5 = plus5;
+        this.resetButton = reset;
+        this.backgroundColor = Color.parseColor("#000000");
+        this.playerName.setTextColor(Color.parseColor("#ffffff"));
+
+        //init onClickListeners
+        this.buttonSit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activatePlayer();
+            }
+        });
+        this.buttonStand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deactivatePlayer();
+            }
+        });
+        this.buttonMinus5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removePoints(5);
+            }
+        });
+        this.buttonMinus1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removePoints(1);
+            }
+        });
+        this.buttonPlus1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addPoints(1);
+            }
+        });
+        this.buttonPlus5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addPoints(5);
+            }
+        });
+        this.resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                score = 0;
+                resetButton.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        //upon creating player, he or she should be set as inactive
+        deactivatePlayer();
+    }
+
+    private void displayScore(int number){
+        this.scoreTextView.setText(String.valueOf(number));
+    }
+    private void setScore(int number){
+        this.score = (number >= 0) ? number : 0; // we don't want score to be less than 0
+        displayScore(this.score);
+    }
+    private void addPoints(int points){
+        setScore(this.score+points);
+    }
+    private void removePoints(int points) {
+        setScore(this.score - points);
+    }
+    private void setActive(boolean setting) {
+        if (!setting) {
+            scoreTextView.setVisibility(View.INVISIBLE);
+            buttonSit.setVisibility(View.VISIBLE);
+            buttonStand.setVisibility(View.GONE);
+            buttonMinus5.setVisibility(View.INVISIBLE);
+            buttonMinus1.setVisibility(View.INVISIBLE);
+            buttonPlus1.setVisibility(View.INVISIBLE);
+            buttonPlus5.setVisibility(View.INVISIBLE);
+            if(score != 0) resetButton.setVisibility(View.VISIBLE);
+            background.setBackgroundColor(Color.GRAY);
+            scoreTextView.setTextColor(Color.parseColor("#ffffff"));
+        } else {
+            scoreTextView.setVisibility(View.VISIBLE);
+            buttonSit.setVisibility(View.GONE);
+            buttonStand.setVisibility(View.VISIBLE);
+            buttonMinus5.setVisibility(View.VISIBLE);
+            buttonMinus1.setVisibility(View.VISIBLE);
+            buttonPlus1.setVisibility(View.VISIBLE);
+            buttonPlus5.setVisibility(View.VISIBLE);
+            resetButton.setVisibility(View.GONE);
+            displayScore(this.score);
+            background.setBackgroundColor(backgroundColor);
+        }
+    }
+    private void activatePlayer(){
+        setActive(true);
+    }
+    private void deactivatePlayer(){
+        setActive(false);
+    }
+    public void setBackgroundColor (String colorString){
+        this.backgroundColor = Color.parseColor(colorString);
+    }
+}
